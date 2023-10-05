3b93b40077309af508caf3be2a8860f240a826f2
mathias-magnusson <mmagdk@gmail.com>
2023-10-05 10:52:04 +0200

    Update README and add .env to gitignore

--- LoadData.py
+++ LoadData.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import os
+
+# /Users/mathiasmagnusson/Zone-routing-protocol/data.csv
+
+FILE_PATH = os.environ.get("MY_PROJECT_FILE_PATH", "data.csv")
 
 headers = [f"sat{i}" for i in range(0, 66)]
-data = pd.read_csv("Zone-routing-protocol/data.csv", names=headers)
+data = pd.read_csv(FILE_PATH, names=headers)
 
 def get_position_data(satIndex: int):
     return data.iloc[:,satIndex]
