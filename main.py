94985fdf3f44dfe74802af45b77cdc2f8fe8455b
DanielHolm17 <Daniel12307@live.dk>
2023-11-16 15:20:09 +0100

    Now running in for loop

--- main.py
+++ main.py
@@ -14,31 +14,41 @@ def sort_table(table):
     sorted_routing = sorted(table.items())
     return dict(sorted_routing)
 
+
+run_time = 2
+sample_time = 1
+
 def network_simulator(env, nodes):
-    start = env.now
+    for i in range(run_time):
+        find_node_neighbours(nodes, i*20)
 
-    for node in nodes:
-        node.routing_table_new.clear()
-        node.metrics_table_new.clear()
-        node.paths_to_destinations.clear()
-        yield env.process(node.iarp())
-        node.routing_table = node.routing_table_new
-        node.metrics_table = node.metrics_table_new
+        start = env.now
 
-        node.routing_table = sort_table(node.routing_table)
-        node.metrics_table = sort_table(node.metrics_table)
+        for node in nodes:
+            node.routing_table_new.clear()
+            node.metrics_table_new.clear()
+            node.paths_to_destinations.clear()
+            yield env.process(node.iarp())
+            node.routing_table = node.routing_table_new
+            node.metrics_table = node.metrics_table_new
 
-        #print(f"Node {node.node_id} routing table: {node.routing_table}\n")
-        #print(f"Node {node.node_id} metric table: {node.metrics_table}\n")
+            node.routing_table = sort_table(node.routing_table)
+            node.metrics_table = sort_table(node.metrics_table)
 
-    source = 0
-    destination = 3
+            #print(f"Node {node.node_id} routing table: {node.routing_table}\n")
+            #print(f"Node {node.node_id} metric table: {node.metrics_table}\n")
 
-    yield env.process(nodes[source].ierp(destination))
-    stop = env.now
-    print(nodes[source].paths_to_destinations)
-    print(nodes[source].get_best_path_ierp(destination, True))
-    print(f"Elapsed time: {stop-start}")
+        source = 0
+        destination = 3
+
+        yield env.process(nodes[source].ierp(destination))
+        stop = env.now
+        print(nodes[source].paths_to_destinations)
+        print(nodes[source].get_best_path_ierp(destination, True))
+        print(f"Elapsed time: {stop-start}")
+
+        #for key, value in nodes[0].routing_table.items():
+        #    print(f"{key}: {value}")
 
 # Create environment
 env = simpy.rt.RealtimeEnvironment(factor=0.1)
@@ -46,33 +56,9 @@ env = simpy.rt.RealtimeEnvironment(factor=0.1)
 # Create nodes
 nodes = []
 zone_radius = 2
-for i in range(22):
+for i in range(66):
     nodes.append(Node.Node(env, i, zone_radius, position=LoadData.get_position_data(i)))
-
-# finding neighbour nodes for all nodes at time: 0
-find_node_neighbours(nodes, 0)
 
 # Run the simulation
 env.process(network_simulator(env,nodes))
 env.run()
-
-
-### Run every sample
-
-"""run_time = 120
-sample_time = 1
-
-def network_simulator(env, nodes):
-    for i in range(run_time):
-
-        find_node_neighbours(nodes,i)
-        
-        for node in nodes:
-            node.routing_table_new.clear()
-            node.metrics_table_new.clear()
-            yield env.process(node.iarp())
-            node.routing_table = node.routing_table_new
-            node.metrics_table = node.metrics_table_new
-            print(f"Node {node.node_id} routing table: {node.routing_table}\n")
-    
-        sleep(sample_time)"""
