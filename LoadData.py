c3b89a0bceb6f63fc190152188e129707366f894
mathias-magnusson <mmagdk@gmail.com>
2023-11-17 13:50:07 +0100

    hygge-test

--- LoadData.py
+++ LoadData.py
@@ -6,7 +6,7 @@ import os
 #FILE_PATH = os.environ.get("/Users/mathiasmagnusson/Zone-routing-protocol/data.csv")
 
 headers = [f"sat{i}" for i in range(0, 66)]
-data = pd.read_csv("/Users/mathiasmagnusson/Zone-routing-protocol/data.csv", names=headers)
+data = pd.read_csv("/Users/mathiasmagnusson/Zone-routing-protocol/mobilityModel_10Hours.csv", names=headers)
 
 
 def get_position_data(satIndex: int):
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import simpy
 import Node
 import LoadData
 import time
+import csv
 
 def find_node_neighbours(nodes: [], index : int):
     for node in nodes:
@@ -15,47 +16,63 @@ def sort_table(table):
     return dict(sorted_routing)
 
 
-run_time = 2
+run_time = 380
 sample_time = 1
 
 def network_simulator(env, nodes):
+
+    data = [[],[]]
     for i in range(run_time):
-        find_node_neighbours(nodes, i*20)
+        find_node_neighbours(nodes=nodes, index=i)
 
         start = env.now
 
-        for node in nodes:
-            node.routing_table_new.clear()
-            node.metrics_table_new.clear()
-            node.paths_to_destinations.clear()
-            yield env.process(node.iarp())
-            node.routing_table = node.routing_table_new
-            node.metrics_table = node.metrics_table_new
+        nodes[0].routing_table_new.clear()
+        nodes[0].metrics_table_new.clear()
+        nodes[0].paths_to_destinations.clear()
+        yield env.process(nodes[0].iarp())
+        nodes[0].routing_table = nodes[0].routing_table_new
+        nodes[0].metrics_table = nodes[0].metrics_table
 
-            node.routing_table = sort_table(node.routing_table)
-            node.metrics_table = sort_table(node.metrics_table)
+        nodes[0].routing_table = sort_table(nodes[0].routing_table)
+        nodes[0].metrics_table = sort_table(nodes[0].metrics_table)
+
+        # for node in nodes:
+        #     node.routing_table_new.clear()
+        #     node.metrics_table_new.clear()
+        #     node.paths_to_destinations.clear()
+        #     yield env.process(node.iarp())
+        #     node.routing_table = node.routing_table_new
+        #     node.metrics_table = node.metrics_table_new
+
+        #     node.routing_table = sort_table(node.routing_table)
+        #     node.metrics_table = sort_table(node.metrics_table)
 
             #print(f"Node {node.node_id} routing table: {node.routing_table}\n")
             #print(f"Node {node.node_id} metric table: {node.metrics_table}\n")
 
         source = 0
-        destination = 3
+        destination = 49
 
-        yield env.process(nodes[source].ierp(destination))
+        #yield env.process(nodes[source].ierp(destination))
         stop = env.now
-        print(nodes[source].paths_to_destinations)
-        print(nodes[source].get_best_path_ierp(destination, True))
-        print(f"Elapsed time: {stop-start}")
+       # print(nodes[source].paths_to_destinations)
+       # print(nodes[source].get_best_path_ierp(destination, True))
+        print(f"Elapsed time for T = {i}: {stop-start}")
 
-        #for key, value in nodes[0].routing_table.items():
-        #    print(f"{key}: {value}")
+        # Write to data
+        data[0].append(i)
+        data[1].append(stop-start) #zone = 2
+    with open('/Users/mathiasmagnusson/Zone-routing-protocol/iarp-test-1.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows(data)
 
 # Create environment
 env = simpy.Environment()
 
 # Create nodes
 nodes = []
-zone_radius = 2
+zone_radius = 1
 for i in range(66):
     nodes.append(Node.Node(env, i, zone_radius, position=LoadData.get_position_data(i)))
 
